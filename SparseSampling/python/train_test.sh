28268d5f7d6fcbe30d572bdbc5de5523523b1fe5
Dylan Paiton <dpaiton@gmail.com>
2016-02-25 16:24:05 -0800

    dont need any more

--- SparseSampling/python/train_test.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env sh
-
-GLOG_logtostderr=0 GLOG_log_dir=checkpoints/log/ /Users/dpaiton/Work/Libraries/caffe/build/tools/caffe train \
-    --solver=solver.prototxt 
-    #--weights=checkpoints/mlp_iter_30000.caffemodel
