54db9b412c6349f7d12509bb69daa95fe42ee112
Dylan Paiton <dpaiton@gmail.com>
2016-03-06 21:14:30 -0800

    fixed number of iterations to run over all data

--- SparseSampling/python/test_net.sh
+++ SparseSampling/python/test_net.sh
@@ -2,5 +2,5 @@
 
 GLOG_logtostderr=0 GLOG_log_dir=checkpoints/log/ /Users/dpaiton/Work/Libraries/caffe/build/tools/caffe test \
     --model=mlp_test.prototxt \
-    --weights=checkpoints/mlp_sparse_iter_100000.caffemodel \
-    --iterations 100
+    --weights=checkpoints/mlp_sparse_v1_iter_100000.caffemodel \
+    --iterations 200
