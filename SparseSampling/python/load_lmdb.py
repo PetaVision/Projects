c5a6ac4d3b0aef179603ac5c2972d9b80963bbb6
Dylan Paiton <dpaiton@gmail.com>
2016-03-11 13:39:58 -0800

    added use case for non-float lmdb files (images)

--- SparseSampling/python/load_lmdb.py
+++ SparseSampling/python/load_lmdb.py
@@ -6,7 +6,10 @@ import IPython
 # CIFAR has 60000 training images
 num_frames = -1 # [int] number of frames to load in (all if <0)
 
-env = lmdb.open('lmdb_datasets/pvCifar_test_lmdb_v1_samples_11', readonly=True)
+#input_file = 'lmdb_datasets/pvCifar_test_lmdb_v1_samples_11'
+input_file = '/Users/dpaiton/Work/Datasets/CIFAR/cifar10-val/cifar10_val_lmdb'
+
+env = lmdb.open(input_file, readonly=True)
 datum = caffe.proto.caffe_pb2.Datum()
 
 x = []
@@ -18,8 +21,12 @@ with env.begin() as txn:
             break
         datum.ParseFromString(value)
         flat_x = np.array(datum.float_data)
-        x.append(flat_x.reshape(datum.channels, datum.height, datum.width))
-        y.append(datum.label)
+        if flat_x.size == 0:
+            x.append(np.fromstring(datum.data, dtype=np.uint8).reshape(datum.channels, datum.height, datum.width))
+            y.append(datum.label)
+        else:
+            x.append(flat_x.reshape(datum.channels, datum.height, datum.width))
+            y.append(datum.label)
         print 'Loaded frame ' + key
 
 x_array = np.vstack([x[i][np.newaxis,:] for i in range(len(x))])
