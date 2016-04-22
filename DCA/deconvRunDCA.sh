9fc074948f72f4569e57839aa20ff36d7ce94c00
Will Shainin <wshainin@gmail.com>
2016-04-22 16:09:32 -0600

    Added projects from OpenPV/projects

---
+++ DCA/deconvRunDCA.sh
@@ -0,0 +1,3 @@
+~/workspace/PVSystemTests/BasicSystemTest/Release/BasicSystemTest -p ~/workspace/projects/DCA/input/DCA/deconvolveS1.params -t 8
+~/workspace/PVSystemTests/BasicSystemTest/Release/BasicSystemTest -p ~/workspace/projects/DCA/input/DCA/deconvolveS2.params -t 8
+~/workspace/PVSystemTests/BasicSystemTest/Release/BasicSystemTest -p ~/workspace/projects/DCA/input/DCA/deconvolveS3.params -t 8
